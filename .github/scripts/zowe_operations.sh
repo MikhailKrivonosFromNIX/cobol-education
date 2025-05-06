32c91acfd521cd3db384c6a7d7cc94792583e21c
MikhailKrivonosFromNIX <mikhailkrivonos96@gmail.com>
2025-05-06 17:43:59 +0200

    Update zowe_operations.sh

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -6,11 +6,11 @@ LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 # Check if directory exists, create if it doesn't
 if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
 	echo "Directory does not exist. Creating it..."
-	zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck
+	zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck --zosmf-profile myprofile123
 else
 	echo "Directory already esizts."
 fi
 
 # Verify upload
 echo "Verifying upload:"
-zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
+zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --zosmf-profile myprofile123
