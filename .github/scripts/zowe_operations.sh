63d2f20510183c44681f8303dfdb15891b6e7880
MikhailKrivonosFromNIX <mikhailkrivonos96@gmail.com>
2025-05-07 15:09:52 +0200

    added direct arguments

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -4,13 +4,19 @@
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 
 # Check if directory exists, create if it doesn't
-if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
+if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --host "$ZOWE_HOST" \
+  --user "$ZOWE_USERNAME" \
+  --password "$ZOWE_PASSWORD" &>/dev/null; then
 	echo "Directory does not exist. Creating it..."
-	zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck
+	zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck --host "$ZOWE_HOST" \
+  --user "$ZOWE_USERNAME" \
+  --password "$ZOWE_PASSWORD"
 else
 	echo "Directory already esizts."
 fi
 
 # Verify upload
 echo "Verifying upload:"
-zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
+zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --host "$ZOWE_HOST" \
+  --user "$ZOWE_USERNAME" \
+  --password "$ZOWE_PASSWORD"
