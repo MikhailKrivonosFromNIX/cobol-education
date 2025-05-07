fccfe5c4c5dca0eaefd2703ea2d9cd1734798246
MikhailKrivonosFromNIX <mikhailkrivonos96@gmail.com>
2025-05-07 15:54:15 +0200

    added unautorised access

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -6,11 +6,11 @@ LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 # Check if directory exists, create if it doesn't
 if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --host "$ZOWE_HOST" \
   --user "$ZOWE_USERNAME" \
-  --password "$ZOWE_PASSWORD" --port 10443 &>/dev/null; then
+  --password "$ZOWE_PASSWORD" --port 10443 --reject-unauthorized false &>/dev/null; then
 	echo "Directory does not exist. Creating it..."
 	zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck --host "$ZOWE_HOST" \
   --user "$ZOWE_USERNAME" \
-  --password "$ZOWE_PASSWORD"  --port 10443
+  --password "$ZOWE_PASSWORD" --port 10443 --reject-unauthorized false
 else
 	echo "Directory already esizts."
 fi
@@ -19,4 +19,4 @@ fi
 echo "Verifying upload:"
 zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --host "$ZOWE_HOST" \
   --user "$ZOWE_USERNAME" \
-  --password "$ZOWE_PASSWORD" --port 10443
+  --password "$ZOWE_PASSWORD" --port 10443 --reject-unauthorized false
