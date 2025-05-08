295bfbdfc2ca294a9d6f2b0bbf0e7d50f381db0e
MikhailKrivonosFromNIX <mikhailkrivonos96@gmail.com>
2025-05-08 11:57:39 +0200

    java

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -28,8 +28,8 @@ run_cobolcheck() {
     echo "Running cobolcheck for $program"
 
     # Run cobolcheck, but don't exit if it fails
-    ./cobolcheck -p $program
-    #java -jar bin/cobol-check-0.2.17.jar -p $program
+    #./cobolcheck -p $program
+    java -jar bin/cobol-check-0.2.17.jar -p $program
     echo "Cobolcheck execution completed for $program (exceptions may have occured)"
 
     # Check if CC##99.CBL was created, regardless of cobolcheck exit status
